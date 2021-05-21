8227fe90dc17c651fdcaacc87d9d0d22b922fc50
DoThiHongThao <hongthao2491999@gmail.com>
2021-05-21 17:01:27 +0700

    save avatar by storage

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,84 +1,107 @@
-
 import firebase from 'firebase';
 
 const firebaseConfig = {
     apiKey: "AIzaSyDVKFBExTbWOEw1yfQ_ZXvKiygdIqFB6Lc",
     authDomain: "fb-sample-e0c7b.firebaseapp.com",
+    databaseURL: "https://fb-sample-e0c7b-default-rtdb.firebaseio.com",
     projectId: "fb-sample-e0c7b",
     storageBucket: "fb-sample-e0c7b.appspot.com",
     messagingSenderId: "434018374093",
     appId: "1:434018374093:web:761e6c336d01a75f783484",
-    measurementId: "G-PXKP2B2FPJ"
+    measurementId: "G-PXKP2B2FPJ"  
 };
 
-firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp(firebaseConfig);
 
 const db = firebase.firestore();
 export const auth = firebase.auth();
 export default firebase;
 
 export const getFirebaseItems = async () => {
-    try {
-      const snapshot = await db
-        .collection("todos")
-        .get();
-      const items = snapshot.docs.map(
-        (doc) => ({ ...doc.data(), id: doc.id })
-      );
-      return items;
-    } catch (err) {
-      console.log(err);
-      return [];
-    }
+  try {
+    const snapshot = await db
+      .collection("todos")
+      .get();
+    const items = snapshot.docs.map(
+      (doc) => ({ ...doc.data(), id: doc.id })
+    );
+    return items;
+  } catch (err) {
+    console.log(err);
+    return [];
+  }
 }
-  
+
 export const addFirebaseItem = async (item) => {
-    try {
-      const todoRef = db.collection("todos");
-      await todoRef.add(item);
-    } catch (err) {
-      console.log(err);
-    }
+  try {
+    const todoRef = db.collection("todos");
+    await todoRef.add(item);
+  } catch (err) {
+    console.log(err);
+  }
 }
-  
+
 export const updateFirebaseItem = async (item, id) => {
-    try {
-      const todoRef = db.collection("todos").doc(id);
-      await todoRef.update(item);
-    } catch (err) {
-      console.log(err);
-    }
+  try {
+    const todoRef = db.collection("todos").doc(id);
+    await todoRef.update(item);
+  } catch (err) {
+    console.log(err);
+  }
 }
-  
+
 export const clearFirebaseItem = async (item) => {
-    const todoRef = db.collection("todos").doc(item.id);
-    await todoRef.delete().then(function () {
-    }).catch(function (err) {
-      console.log(err);
-    });
+  const todoRef = db.collection("todos").doc(item.id);
+  await todoRef.delete().then(function () {
+  }).catch(function (err) {
+    console.log(err);
+  });
 };
 
 export const uiConfig = {
-    signInFlow: 'popup',
-    signInSuccessUrl: "/",
-    signInOptions: [
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    ],
-  }
-  
-export const storeUserInfo = async (user) => {
-    const { uid } = user;
-    const userDoc = await db.collection("users").doc(uid).get();
-    if (!userDoc.exists) {
-      await db.collection("users").doc(uid).set({ name: user.displayName });
-      return {
-        name: user.displayName,
-        id: uid,
-      };
-    } else {
-      return {
-        id: uid,
-        ...userDoc.data(),
-      };
-    }
+  signInFlow: 'popup',
+  signInSuccessUrl: "/",
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+  ],
 }
+
+export const storeUserInfo = async (user) => {
+  const { uid } = user; 
+  const userDoc = await db.collection("users").doc(uid).get();
+  if (!userDoc.exists) {
+    await db.collection("users").doc(uid).set({ name: user.displayName });
+    return {
+      name: user.displayName,
+      id: uid,
+    };
+  } else {
+    return {
+      id: uid,
+      ...userDoc.data(),
+    };
+  }
+}
+
+export const updateUser = async (user, image) => {
+  try {
+    const userDoc = await firebase.firestore().collection("users").doc(user.id).get();
+    if (userDoc.exists) {
+      await firebase.firestore().collection("users").doc(user.id).update({ ...userDoc.data(), image: image });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export const uploadImage = async (image) => {
+  const ref = firebase.storage().ref().child(`/images/${image.name}`);
+  let downloadUrl = "";
+  try {
+    await ref.put(image);
+    downloadUrl = await ref.getDownloadURL();
+  } catch (err) {
+    console.log(err);
+  }
+  return downloadUrl;
+};
