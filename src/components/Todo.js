1f329e1767fb779b8612cc839cdb00c4370d6713
DoThiHongThao <hongthao2491999@gmail.com>
2021-05-21 16:20:53 +0700

    save todo on firestore

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,94 +1,79 @@
 import React, { useState } from 'react';
 
-/* 
-  【Todoのデータ構成】
-　・key：Todoを特定するID（String）
-　・text：Todoの内容（String）
-　・done：完了状態（Boolean true:完了済み,, false:未完了）
-*/
-
 /* コンポーネント */
 import TodoItem from './TodoItem';
 import Input from './Input';
 import Filter from './Filter';
 
 /* カスタムフック */
-import useStorage from '../hooks/storage';
+// import useStorage from '../hooks/storage';
+import useFbStorage from '../hooks/fbStorage';
 
 /* ライブラリ */
 import {getKey} from "../lib/util";
 
 function Todo() {
-  const [items, putItems, clearItems] = useStorage([]);
+  // const [items, putItems, clearItems] = useStorage();
+  const [items, addItem, updateItem, clearItems] = useFbStorage();
   
-  const [tab,setTab] = useState("すべて");
+  const [filter, setFilter] = React.useState('ALL');
+
+  const displayItems = items.filter(item => {
+    if (filter === 'ALL') return true;
+    if (filter === 'TODO') return !item.done;
+    if (filter === 'DONE') return item.done;
+  });
   
-  const itemTab = () => {
-      const tabItem = items.filter((item) => {
-          if (tab === "すべて")
-              return item;
-          if (tab === "未完了" && !item.done) {
-              return item;
-          }
-          if (tab === "完了済み" && item.done) {
-              return item;
-          }
-      });
-      return tabItem;
+  const handleCheck = checked => {
+    // const newItems = items.map(item => {
+    //   if (item.key === checked.key) {
+    //     item.done = !item.done;
+    //   }
+    //   return item;
+    // });
+    // putItems(newItems);
+    updateItem(checked);
   };
-  const handleChangeTab = (target) =>{
-      setTab(target);
+  
+  const handleAdd = text => {
+    // putItems([...items, { key: getKey(), text, done: false }]);
+    addItem({ text, done: false });
   };
-const onChange = (e) => {
-  if(e.key === 'Enter' && e.target.value){
-    const newItem =  {
-      key: getKey(),
-      text: e.target.value,
-      done: false
-    }
-    
-    items.push(newItem);
-    
-    putItems([...items]);
-  }
-}
-
-const onClickBox = (key) => {
-    items.map(item => {
-      if(item.key === key) {
-        item.done = !item.done;
-      }
-    });
-    putItems([...items]);
-  }
-
-  const todo = itemTab().map(item => (
-    <TodoItem 
-         key={item.key}
-         item={item}
-         onClickBox={onClickBox}
-    />
-  ));
+  
+  const handleFilterChange = value => setFilter(value);
 
   return (
-    <div className="panel">
+    <article class="panel is-danger">
       <div className="panel-heading">
-        ITSS ToDoアプリ
+        <span class="icon-text">
+          <span class="icon">
+            <i class="fas fa-calendar-check"></i>
+          </span>
+          <span> ITSS Todoアプリ</span>
+        </span>
       </div>
-      <label>
-        <input className="input" type="text" onKeyDown={(e) => onChange(e)}></input>
-      </label>
-      <br/>
-      <Filter onClick={handleChangeTab}/>
-      {todo}
+      <Input onAdd={handleAdd} />
+      <Filter
+        onChange={handleFilterChange}
+        value={filter}
+      />
+      {displayItems.map(item => (
+        <TodoItem 
+          key={item.id}
+          item={item}
+          onCheck={handleCheck}
+        />
+      ))}
       <div className="panel-block">
-        {itemTab().length} items
+        {displayItems.length} items
       </div>
       <div className="panel-block">
-        <button className="btn btn-primary" onClick={clearItems} >CLEAR</button>
+        <button className="button is-light is-fullwidth" onClick={clearItems}>
+          全てのToDoを削除
+        </button>
       </div>
-    </div>
-    );
+    </article>
+  );
 }
 
 export default Todo;
