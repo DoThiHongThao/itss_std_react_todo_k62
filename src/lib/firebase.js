4b96b803bce681e78ecded6bf2e1c9a0ba584921
DoThiHongThao <hongthao2491999@gmail.com>
2021-05-21 16:12:40 +0700

    use firebase

---
+++ src/lib/firebase.js
@@ -0,0 +1,14 @@
+
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDVKFBExTbWOEw1yfQ_ZXvKiygdIqFB6Lc",
+    authDomain: "fb-sample-e0c7b.firebaseapp.com",
+    projectId: "fb-sample-e0c7b",
+    storageBucket: "fb-sample-e0c7b.appspot.com",
+    messagingSenderId: "434018374093",
+    appId: "1:434018374093:web:761e6c336d01a75f783484",
+    measurementId: "G-PXKP2B2FPJ"
+};
+
+firebase.initializeApp(firebaseConfig);
